78b9b14f0d0321d4a860eb0cbd0d344b8e3baabc
encoderuz <abduqodirxojiyev@gmail.com>
2023-03-23 10:37:38 +0500

    Make commands created!

--- artisan-doskeys.cmd
+++ artisan-doskeys.cmd
@@ -11,6 +11,8 @@ doskey tinker=php artisan tinker $*
 doskey up=php artisan up $*
 doskey ui=php artisan ui $*
 
+doskey artisan_about=php artisan about $*
+doskey artisan_list=php artisan list $*
 
 doskey auth_reset=php artisan  auth:clear-resets $*
 
@@ -30,7 +32,7 @@ doskey db_table=php artisan db:table $*
 doskey db_wipe=php artisan db:wipe $*
 
 
-doskey debugbar_clear=php artisan debugbar:clear
+doskey debugbar_clear=php artisan debugbar:clear $*
 
 doskey env_decrypt=php artisan env:decrypt $*
 doskey env_encrypt=php artisan env:encrypt $*
@@ -39,14 +41,55 @@ doskey event_cache=php artisan event:cache $*
 doskey event_clear=php artisan event:clear $*
 doskey event_generate=php artisan event:generate $*
 doskey event_list=php artisan event:list $*
+doskey event=php artisan event $*
 
+doskey key_generate=php artisan key:generate $*
+doskey lang_publish=php artisan lang:publish $*
 
 doskey vendor_publish=php artisan vendor:publish $*
 
 
  
-doskey event=php artisan event:list $*
+
 doskey make=php artisan make$*
+doskey make_cast=php artisan make:cast $*
+doskey make_channel=php artisan make:channel $*
+doskey make_command=php artisan make:command $*
+doskey make_component=php artisan make:component $*
+doskey make_controller=php artisan make:controller $*
+doskey make_event=php artisan make:event $*
+doskey make_exception=php artisan make:exception $*
+doskey make_factory=php artisan make:factory $*
+doskey make_job=php artisan make:job $*
+doskey make_listener=php artisan make:listener $*
+doskey make_mail=php artisan make:mail $*
+doskey make_middleware=php artisan make:middleware $*
+doskey make_migration=php artisan make:migration $*
+doskey make_model=php artisan make:model $*
+doskey make_notification=php artisan make:notification $*
+doskey make_observer=php artisan make:observer $*
+doskey make_policy=php artisan make:policy $*
+doskey make_provider=php artisan make:provider $*
+doskey make_request=php artisan make:request $*
+doskey make_resource=php artisan make:resource $*
+doskey make_rule=php artisan make:rule $*
+doskey make_scope=php artisan make:scope $*
+doskey make_seeder=php artisan make:seeder $*
+doskey make_test=php artisan make:test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 doskey route=php artisan route $* 
@@ -68,14 +111,10 @@ doskey config_cc=php artisan config:cache $T php artisan config:clear $*
 
 
 
-doskey key_generate=php artisan key:generate $*
-doskey lang_publish=php artisan lang:publish $*
 
 
 
-doskey artisan_about=php artisan about
-doskey artisan_list=php artisan list
-doskey artisan_list=php artisan list
+
 
 
 
